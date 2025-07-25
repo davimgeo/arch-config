c41d522e59f690395951e37bf40c99282ea53f47
archlum <archlum@archlum.com>
2025-07-24 23:13:13 -0300

    added wallpaper folder

--- backup_configs.sh
+++ backup_configs.sh
@@ -9,13 +9,13 @@ rm -fr dotfiles/dwm || cp -r ~/.config/dwm dotfiles/ && printf "$OK Copied dwm f
 rm -fr dotfiles/alacritty || cp -r ~/.config/alacritty dotfiles/ && printf "$OK Copied alacritty files successfully into dotfiles/\n" || \
 	printf "$FAILED Could not copy alacritty files into dotfiles/\n"
 
-rm -fr dotfiles/.zshrc || cp ~/.zshrc dotfiles/ && printf "$OK Copied zshrc file sucessfully into dotfiles\n" || \
+rm -fr dotfiles/.zshrc || cp ~/.zshrc dotfiles/ && printf "$OK Copied zshrc file sucessfully into dotfiles/\n" || \
 	printf "$FAILED Could not copy zshrc file into dotfiles/\n"
 
-rm -fr dotfiles/.xinitrc || cp ~/.xinitrc dotfiles/ && printf "$OK Copied xinirc file sucessfully into dotfiles\n" || \
+rm -fr dotfiles/.xinitrc || cp ~/.xinitrc dotfiles/ && printf "$OK Copied xinirc file sucessfully into dotfiles/\n" || \
 	printf "$FAILED Could not copy xinitrc file into dotfiles/\n"
 
-rm -fr dotfiles/.zprofile || cp ~/.zprofile dotfiles/ && printf "$OK Copied zprofile file sucessfully into dotfiles\n" || \
+rm -fr dotfiles/.zprofile || cp ~/.zprofile dotfiles/ && printf "$OK Copied zprofile file sucessfully into dotfiles/\n" || \
 	printf "$FAILED Could not copy zprofile file into dotfiles/\n"
 
 
