8df29235db1a74827d16efb54520ed8cf26e0a2d
lum <lum@lum>
2025-08-17 18:44:37 -0300

    updating nvim config

--- dots/nvim/lua/core/plugin_config/gruvbox.lua
+++ dots/nvim/lua/core/plugin_config/gruvbox.lua
@@ -2,11 +2,17 @@ function set_colorscheme(color)
   color = color or "gruvbox"
   vim.cmd.colorscheme(color)
 
-  vim.o.termguicolors = true
-  vim.o.background = "dark"
+ -- vim.o.termguicolors = true
+ -- vim.o.background = "dark"
 
+--  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+  vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+  vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+  vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 end
 
 set_colorscheme()
