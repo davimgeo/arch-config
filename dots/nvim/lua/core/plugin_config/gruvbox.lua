e35c1792cb0b5632b3ef43e5bc2e641db764fb2c
archlum <archlum@arch.com>
2025-08-18 00:59:38 -0300

    new nvim config

--- dots/nvim/lua/core/plugin_config/gruvbox.lua
+++ dots/nvim/lua/core/plugin_config/gruvbox.lua
@@ -1,3 +1,12 @@
-vim.o.termguicolors = true
-vim.o.background = "dark"
-vim.cmd[[ colorscheme gruvbox ]]
+function set_colorscheme(color)
+  color = color or "gruvbox"
+  vim.cmd.colorscheme(color)
+
+  vim.o.termguicolors = true
+  vim.o.background = "dark"
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+set_colorscheme()
--- dots/nvim/lua/core/plugins.lua
+++ dots/nvim/lua/core/plugins.lua
@@ -1,36 +1,41 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
-local packer_bootstrap = ensure_packer()
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'ellisonleao/gruvbox.nvim'
   use 'nvim-tree/nvim-tree.lua'
   use 'nvim-tree/nvim-web-devicons'
-  use 'nvim-lualine/lualine.nvim'
-  use 'nvim-treesitter/nvim-treesitter'
+  use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
   use {
-    'nvim-telescope/telescope.nvim',
-     tag = '0.1.0',
-     requires = { { 'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
   }
 
-  -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
 end)
