2d4b1cdc3b72883a5a1ed34a167ccb8daf7ea939
archlum <archlum@arch.com>
2025-08-17 22:52:37 -0300

    refactored backup script

--- backup_configs.sh
+++ backup_configs.sh
@@ -1,37 +1,42 @@
 #!/bin/zsh
+# *****************
+# Backup dotfiles
+# *****************
 
-OK="[ \033[1;32mOK\033[0m ]"
-FAILED="[ \033[1;31mFailed\033[0m ]"
+# array of configs in .config dir 
+CONFIG_DIRS=(
+  i3      nvim     alacritty
+)
 
-mkdir -p dots
+# array of configs in home dir
+HOME_DIRS = (
+  .zshrc .zprofile .xinitrc
+)
 
-# I3
-rm -fr dots/i3 && cp -r ~/.config/i3 dots/ && \
-  printf "$OK Copied I3 files successfully into dots/\n" || \
-  printf "$FAILED Could not copy I3 files into dots/\n"
+BACKUP_DIR="dots"
 
-# nvim
-rm -fr dots/nvim && cp -r ~/.config/nvim dots/ && \
-  printf "$OK Copied nvim files successfully into dots/\n" || \
-  printf "$FAILED Could not copy nvim files into dots/\n"
+function backup_item() {
+  # colored msgs for debug
+  local OK="[ \033[1;32mOK\033[0m ]"
+  local FAILED="[ \033[1;31mFailed\033[0m ]"
 
-# Alacritty
-rm -fr dots/alacritty && cp -r ~/.config/alacritty dots/ && \
-  printf "$OK Copied alacritty files successfully into dots/\n" || \
-  printf "$FAILED Could not copy alacritty files into dots/\n"
+  local src=$1
 
-# .zshrc
-rm -f dots/.zshrc && cp ~/.zshrc dots/ && \
-  printf "$OK Copied zshrc file successfully into dots/\n" || \
-  printf "$FAILED Could not copy zshrc file into dots/\n"
+  rm -rf "$BACKUP_DIR/$src"
+  cp -r "$src" "$BACKUP_DIR/"
 
-# .xinitrc
-rm -f dots/.xinitrc && cp ~/.xinitrc dots/ && \
-  printf "$OK Copied xinitrc file successfully into dots/\n" || \
-  printf "$FAILED Could not copy xinitrc file into dots/\n"
+  if [[ $? -eq 0 ]]; then
+    printf "$OK Copied $src files successfully into $BACKUP_DIR/\n" \
+    || printf "$FAILED Could not copy $src files into $BACKUP_DIR/\n"  
+  fi
+}
 
-# .zprofile
-rm -f dots/.zprofile && cp ~/.zprofile dots/ && \
-  printf "$OK Copied zprofile file successfully into dots/\n" || \
-  printf "$FAILED Could not copy zprofile file into dots/\n"
+# backup configs in .config/ directories
+for dot in "${config_dirs[@]}"; do
+  backup_item "$HOME/.config/$dot"
+done
 
+# backup configs in home/ dir
+for dot in "${HOME_DIRS[@]}"; do 
+  backup_item "$HOME/$dot"
+done
