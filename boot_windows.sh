365d64b8eaebf6b15735ab4347aab54d68f3a2e9
lum <lum@lum>
2026-05-13 03:29:52 -0500

    added windows boot order

--- boot_windows.sh
+++ boot_windows.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-WINDOWS_BOOT_NUMBER=sudo efibootmgr | grep "Windows" | sed -E 's/Boot([0-9A-F]{4}).*/\1/'
+WINDOWS_BOOT_NUMBER=$(sudo efibootmgr | grep "Windows" | sed -E 's/Boot([0-9A-F]{4}).*/\1/')
 
-echo -n "Do you want to boot into the following input?\n"
-read answer
-
+echo -e "Do you want to boot into the following input? (Y/N)\n"
 sudo efibootmgr | grep "Windows"
 
-if [[ ! "$answer" =~ ^([yY]|[yY][eE][sS])$ ]]; then
-  sudo efibootmgr -n WINDOWS_BOOT_NUMBER
+read -r answer
 
+if [[ "$answer" =~ ^([yY]|[yY][eE][sS])$ ]]; then
+  sudo efibootmgr -n "$WINDOWS_BOOT_NUMBER"
   sudo reboot
 else
-  echo "Stoping program..."
+  echo "Stopping program..."
 fi
 
